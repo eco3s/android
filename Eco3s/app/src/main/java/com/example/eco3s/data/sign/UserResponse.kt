6a39206805c786741ef529c1179250f56e7a76e7
sinbee0402 <osb3476@gmail.com>
2022-09-26 14:31:14 +0900

    feat : Update UserResponse
    UserResponse 수정

--- Eco3s/app/src/main/java/com/example/eco3s/data/sign/UserResponse.kt
+++ Eco3s/app/src/main/java/com/example/eco3s/data/sign/UserResponse.kt
@@ -1,7 +1,12 @@
 package com.example.eco3s.data.sign
 
+import com.google.gson.annotations.SerializedName
+
 data class UserResponse(
-    val id: String?,
+    @SerializedName("id")
+    val ObjectId: String?,
+    @SerializedName("name")
     val name: String?,
-    val createAt: Int
+    @SerializedName("createdAt")
+    val Date: Int
 )
